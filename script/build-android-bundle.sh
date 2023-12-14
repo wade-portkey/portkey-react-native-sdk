319859af834600b9c84ae163357fa89af83aa499
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2023-12-14 15:34:08 +0800

    fix: compile

--- script/build-android-bundle.sh
+++ script/build-android-bundle.sh
@@ -1,10 +1,10 @@
 # build index.android.bundle
 SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR/../"
-if [ ! -d "android/src/main/assets" ]; then
-  mkdir -p android/src/main/assets
+if [ ! -d "android/src/main/assets/js" ]; then
+  mkdir -p android/src/main/assets/js
 fi
-if [ ! -f "android/src/main/assets/index.android.bundle" ]; then
-  touch android/src/main/assets/index.android.bundle
+if [ ! -f "android/src/main/assets/js/index.android.bundle" ]; then
+  touch android/src/main/assets/js/index.android.bundle
 fi
-react-native bundle --platform android --entry-file src/index.ts --bundle-output android/src/main/assets/index.android.bundle --assets-dest android/src/main/res/  --dev true
+react-native bundle --platform android --entry-file src/index.ts --bundle-output android/src/main/assets/js/index.android.bundle --assets-dest android/src/main/assets/js/res  --dev true
